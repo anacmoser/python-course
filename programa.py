bf1f52e61b1c6460abccc975613a7b92aaa499c3
Ana Julia Cardoso Moser <cardosomoseranajulia@gmail.com>
2024-05-19 17:59:36 -0300

    primeira semana

---
+++ programa.py
@@ -0,0 +1,3 @@
+print("mensagem")
+print ("Hello World!")
+print ("hi how are you guys? this is my first time using python! i'm very happy that i did all this by myself in my house(ok i did had virtual classes but i'm stil very proud")
